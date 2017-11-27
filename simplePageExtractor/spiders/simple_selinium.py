8254070f7973889e132a10f49b931144c9385222
Bala Vineeth Netha Thatipamula <balavnth@gmail.com>
2017-11-27 12:10:08 -0500

    Added Comments for the Scrapy-Spider Class Functions

--- simplePageExtractor/spiders/simple_selinium.py
+++ simplePageExtractor/spiders/simple_selinium.py
@@ -13,27 +13,35 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+#Scrapy Class
 class CrawlerSpider(scrapy.Spider):
+    #Spider Name
     name = "selspider"
+    #Giving List of Starting URLs
     start_urls = ['http://forum.nafc.org/login/']
 
+    #Constructor Class - Initializing the Firefox Web Driver
     def __init__(self, filename=None):
         self.driver = webdriver.Firefox()
         dispatcher.connect(self.spider_closed, signals.spider_closed)
 
+    #Close the Spider
     def spider_closed(self, spider):
         self.driver.close()
 
+    #Main Parse function for getting the urls
     def parse(self, response):
         # Load the current page into Selenium
         self.driver.get(response.url)
         username = self.driver.find_element_by_id('registerUserName')
         password = self.driver.find_element_by_id('registerPass')
 
-        username.send_keys('helloworld123')
-        password.send_keys('helloworld123')
+        #Giving the Username and Password
+        username.send_keys("username")
+        password.send_keys("passwd")
 
         self.driver.find_element_by_name("registerButton").click()
+
         #Click the Show More Button for 5 times
         for i in range(5):
             self.driver.find_element_by_xpath('//div[@id="moreButton"]/div[@class="moreThreads"]/a').click()
@@ -45,4 +53,3 @@ class CrawlerSpider(scrapy.Spider):
         # scrape as normal
         url_list = response.xpath('//a[contains(@href, "topic")]/@href').extract()
         self.log(url_list)
-        
